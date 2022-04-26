d3cc058ba2cc7cd28c2d8700b7897a80168aa6c1
Lekozhe <v.a.kozh@ya.ru>
2022-04-26 15:56:50 +0300

    filter by author on click badge

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Test project for Paraweb studio
 
 by Valeria Kozhevnikova
+
+## Features:
+
+- filtering by select element
+- filtering by clicking badge
--- index.js
+++ index.js
@@ -15,23 +15,20 @@ const month = [
   "November",
   "December",
 ];
-const authors = new Set();
-const date = new Set();
-const showAll = "Show all";
+const SHOWALL = "Select author";
+const NONAME = "no name";
 let articlesList = [];
 
 fetch("https://mocki.io/v1/a5814d24-4e22-49fc-96d1-0e9ae2952afc")
   .then((res) => res.json())
   .then(({ articles }) => {
-    articlesList = articles;
     articles.forEach((article) => {
-      authors.add(article.author);
-      date.add(article.publishedAt);
+      article.author = article.author == undefined ? NONAME : article.author;
 
       renderArticle(article);
       renderAuthorOption(article.author);
     });
-    articles;
+    articlesList = articles;
   })
   .catch((error) => console.log(error));
 
@@ -39,13 +36,14 @@ const getTemplate = () =>
   cardTemplate.content.querySelector(".element").cloneNode(true);
 
 const setCardData = (card, { title, description, author, publishedAt }) => {
+  const authorElement = card.querySelector(".element__badge_author");
   let date = new Date(publishedAt);
   date = `${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`;
   card.querySelector(".element__title").innerText = title;
   card.querySelector(".element__description").innerText = description;
-  card.querySelector(".element__badge_author").innerText =
-    author == undefined ? "no name" : author;
   card.querySelector(".element__badge_date").innerText = date;
+  authorElement.innerText = author;
+  authorElement.addEventListener("click", handleClickAuthor);
 };
 
 const renderArticle = (article) => {
@@ -60,14 +58,23 @@ const renderAuthorOption = (author) => {
   authorFilter.appendChild(option);
 };
 
+const handleClickAuthor = (e) => {
+  const selectedAuthor = e.target.innerText;
+  filterAuthor(selectedAuthor);
+  authorFilter.value = selectedAuthor;
+};
+
 const handleFilter = (e) => {
-  const selectedAuthor = e.target.value;
+  filterAuthor(e.target.value);
+};
+
+const filterAuthor = (selectedAuthor) => {
   cardsContainer.innerHTML = "";
   let filteringData = articlesList;
-  if (selectedAuthor !== showAll)
+  if (selectedAuthor !== SHOWALL)
     filteringData = articlesList.filter((c) => c.author === selectedAuthor);
   filteringData.forEach((article) => renderArticle(article));
 };
 
 authorFilter.addEventListener("change", handleFilter);
-renderAuthorOption(showAll);
+renderAuthorOption(SHOWALL);
